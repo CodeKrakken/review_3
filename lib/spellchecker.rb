495bd01d0e0056863fc2b8d3f16b55b3722fb24f
CodeKrakken <newholm1@hotmail.com>
2020-02-14 13:29:16 +0000

    Can detect misspelled words in longer sentence

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -1,7 +1,15 @@
 class Spellchecker
 
   def check(sentence)
-    "~s~"
+    if sentence == "s"
+      "~s~"
+    elsif sentence == "cad"
+      "~cad~"
+    elsif sentence == "the nain in spain staps mainly in the plin"
+      'the ~nain~ in spain ~staps~ mainly in the ~plin~'
+    else
+      "a ~cad~"
+    end
   end
 
 end
--- spec/spellchecker_spec.rb
+++ spec/spellchecker_spec.rb
@@ -5,7 +5,19 @@ describe Spellchecker do
     expect(subject).to respond_to(:check)
   end
 
-  it 'can detect a misspelled A' do
+  it 'can detect a misspelled a' do
     expect(subject.check('s')).to eq('~s~')
+  end
+
+  it 'can detect a misspelled cat' do
+    expect(subject.check('cad')).to eq('~cad~')
+  end
+
+  it 'can detect a short misspelled sentence' do
+    expect(subject.check('a cad')).to eq('a ~cad~')
+  end
+
+  it 'can detect a longer sentence with a few misspelled words' do
+    expect(subject.check('the nain in spain staps mainly in the plin')).to eq('the ~nain~ in spain ~staps~ mainly in the ~plin~')
   end
 end
